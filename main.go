16ba2e11706321f5b331c6b840f1662a54d05350
otofu-square <yukita@feedforce.jp>
2018-01-16 17:02:07 +0900

    :tada: Implement hello handler

--- main.go
+++ main.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func HandleRequest(ctx context.Context, name string) (string, error) {
-	return fmt.Sprintf("Hello %s!", name), nil
+type RequestJSON struct {
+	Name string `json:"name"`
+}
+
+type ResponseJSON struct {
+	Message string `json:"message"`
+}
+
+func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var requestJSON RequestJSON
+	if err := json.Unmarshal([]byte(request.Body), &requestJSON); err != nil || requestJSON.Name == "" {
+		return events.APIGatewayProxyResponse{Body: "Bad Request", StatusCode: 400}, nil
+	}
+
+	greeting := "Hello, " + requestJSON.Name
+	responseJSON, _ := json.Marshal(ResponseJSON{Message: greeting})
+	return events.APIGatewayProxyResponse{Body: string(responseJSON), StatusCode: 200}, nil
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	lambda.Start(handleRequest)
 }
