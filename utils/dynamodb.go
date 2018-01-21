38352196aca7d283ed9894a90e93ea383ec05de7
otofu-square <yukita@feedforce.jp>
2018-01-21 15:15:17 +0900

    :+1: Add utils package

---
+++ utils/dynamodb.go
@@ -0,0 +1,16 @@
+package util
+
+import (
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/guregu/dynamo"
+)
+
+func DynamoDB() dynamo.Table {
+	db := dynamo.New(session.New(), &aws.Config{
+		Region: aws.String("ap-northeast-1"),
+	})
+	return db.Table(os.Getenv("TABLE_NAME"))
+}
